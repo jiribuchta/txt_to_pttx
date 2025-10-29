3d192ccb367c2bf94d93d585069649027b2e1627
Jiří Buchta <jiribuchta02@gmail.com>
2025-10-29 12:08:06 +0100

    added auto generating needed files and change of variable

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ prs = Presentation()
 prs.slide_height = Inches(9)
 prs.slide_width = Inches(16)
 
+if not os.path.exists("./text"):
+    os.makedirs("./text")
+
+if not os.path.exists("./settings.txt"):
+    with open("./settings.txt", 'w', encoding='utf-8') as settings_file:
+        settings_file.write("font_size=72\n")
+
+if os.listdir("./text") == []:
+    print("Please add text files to the 'text' directory and run the script again.")
+    input("Press Enter to exit...")
+
 with open("./settings.txt", 'r', encoding='utf-8') as settings_file:
     settings = settings_file.readlines()
     for setting in settings:
@@ -18,15 +29,14 @@ with open("./settings.txt", 'r', encoding='utf-8') as settings_file:
 for text in os.listdir("./text"):
     with open(os.path.join("./text", text), 'r', encoding='utf-8') as file:
         content = file.read()
-        sloky = content.split("\n\n")
+        verse = content.split("\n\n")
 
-        for s in sloky:
+        for s in verse:
             title_slide_layout = prs.slide_layouts[6] # Blank slide layout
             slide = prs.slides.add_slide(title_slide_layout)
 
             txBox = slide.shapes.add_textbox(Inches(1), Inches(1), Inches(16 - 2), Inches(9 - 2))
             tf = txBox.text_frame
-            #tf.auto_size = MSO_AUTO_SIZE.SHAPE_TO_FIT_TEXT
 
             p = tf.add_paragraph()
             p.font.size = Pt(font_size)
